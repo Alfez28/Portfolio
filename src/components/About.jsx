eb226240d864583cf74f6679b20b19eaca343bff
Alfez28 <alfezkhanujn@gmail.com>
2025-05-30 18:26:12 +0530

    third commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,29 +6,29 @@ export default function About() {
             <div className="aboutcontain w-75 mx-5 ">
                 <h1>About</h1>
                 <p>
-                    Hi there! I'm Alfez Khan, a passionate web developer dedicated to creating functional, 
-                    user-friendly, and visually appealing websites. With a solid background in front-end and 
-                    back-end development, I specialize in turning complex problems into simple, intuitive solutions 
+                    Hi there! I'm <span>Alfez Khan</span>, a passionate web developer dedicated to creating functional, 
+                    user-friendly, and visually appealing websites. With a solid background in <span>front-end</span> and  
+                    <span> back-end</span> development, I specialize in turning complex problems into simple, intuitive solutions 
                     that help businesses and individuals thrive in the digital world.
                     <br/>
                     <br/>
-                    I have a deep understanding of modern web technologies, including HTML, CSS, JavaScript, and 
-                    frameworks like React. On the back-end, I work with languages such as Node.js, building scalable 
-                    and efficient systems. I also have experience with databases like MongoDB.
+                    I have a deep understanding of modern web technologies, including <span>HTML, CSS, JavaScript,</span> and 
+                    frameworks like <span>React JS</span>. On the back-end, I work with languages such as <span>Node JS</span>, building scalable 
+                    and efficient systems. I also have experience with databases like <span>MongoDB</span>.
                     <br/>
                     <br/>
                     Whether you need a responsive, mobile-first website, a custom eCommerce solution, or a web 
-                    application, I'm here to bring your ideas to life. I am committed to delivering clean, 
+                    application, <span>I'm here to bring your ideas to life.</span> I am committed to delivering clean, 
                     well-documented code, as well as ensuring that the final product meets your goals and exceeds 
                     expectations.
                     <br/>
                     <br/>
-                    I believe in the power of collaboration and enjoy working closely with clients to ensure their 
-                    vision is realized. I'm also a constant learner, keeping up with industry trends and best 
+                    <span>I believe in the power of collaboration and enjoy working closely with clients to ensure their 
+                    vision is realized.</span> I'm also a constant learner, keeping up with industry trends and best 
                     practices to ensure that my projects are always on the cutting edge.
                     <br/>
                     <br/>
-                    Feel free to check out my portfolio and get in touch if you’re interested in working together!
+                    <span>Feel free to check out my portfolio and get in touch if you’re interested in working together!</span>
                 </p>
             </div>
         </div>
--- src/components/Myskills.jsx
+++ src/components/Myskills.jsx
@@ -24,42 +24,42 @@ export default function Myskills() {
                     data-aos-delay="500">
                     <img src="./assets/img/js.png" alt="" />
                     <br /><br />
-                    <h4>JAVA SCRIPT</h4>
+                    <h4>Java Script</h4>
                 </div>
                 <div className="bootstrap box" data-aos="fade-up"
                     data-aos-duration="2200"
                     data-aos-delay="600">
                     <img src="./assets/img/bootstrap.png" alt="" />
                     <br /><br />
-                    <h4>BOOTSTRAP</h4>
+                    <h4>Bootstrap</h4>
                 </div>
                 <div className="reactjs box" data-aos="fade-up"
                     data-aos-duration="2400"
                     data-aos-delay="700">
                     <img src="./assets/img/reactjs.png" alt="" />
                     <br /><br />
-                    <h4>REACT JS</h4>
+                    <h4>React JS</h4>
                 </div>
                 <div className="bootstrap box" data-aos="fade-up"
                     data-aos-duration="2600"
                     data-aos-delay="800">
                     <img src="./assets/img/nodejsc.png" alt="" />
                     <br /><br />
-                    <h4>NodeJS</h4>
+                    <h4>Node JS</h4>
                 </div>
                 <div className="bootstrap box" data-aos="fade-up"
                     data-aos-duration="2800"
                     data-aos-delay="900">
                     <img src="./assets/img/expresjs.png" alt="" />
                     <br /><br />
-                    <h4>ExpresJS</h4>
+                    <h4>Expres JS</h4>
                 </div>
                 <div className="bootstrap box" data-aos="fade-up"
                     data-aos-duration="3000"
                     data-aos-delay="1000">
                     <img src="./assets/img/mongodb.png" alt="" />
                     <br /><br />
-                    <h4>MongoDB</h4>
+                    <h4>Mongo DB</h4>
                 </div>
             </div>
 
