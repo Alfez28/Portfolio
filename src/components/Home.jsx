a8bb42ccd5dfc9221b24edad9dd8e25f542c3a9c
Alfez28 <alfezkhanujn@gmail.com>
2025-05-30 16:57:59 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <>
             <div className='home'>
 
-                <div className="container col-xxl-8 px-4 py-5">
+                <div className="container col-xxl-10 px-4 py-4">
                     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                         <div className="himg col-10 col-sm-8 col-lg-6" data-aos="zoom-in"
                             data-aos-duration="2000">
--- src/components/Myskills.jsx
+++ src/components/Myskills.jsx
@@ -6,39 +6,60 @@ export default function Myskills() {
             <h1>My Skills</h1>
             <div className="boxs  d-flex  p-5">
                 <div className="html box" data-aos="fade-up"
-                data-aos-duration="1000"
-                data-aos-delay="300">
+                    data-aos-duration="1600"
+                    data-aos-delay="300">
                     <img src="./assets/img/html.png" alt="" />
                     <br /><br />
                     <h4>HTML</h4>
                 </div>
                 <div className="css box" data-aos="fade-up"
-                data-aos-duration="2000"
-                data-aos-delay="400">
+                    data-aos-duration="1800"
+                    data-aos-delay="400">
                     <img src="./assets/img/css.png" alt="" />
                     <br /><br />
                     <h4>CSS</h4>
                 </div>
                 <div className="js box" data-aos="fade-up"
-                data-aos-duration="3000"
-                data-aos-delay="500">
+                    data-aos-duration="2000"
+                    data-aos-delay="500">
                     <img src="./assets/img/js.png" alt="" />
                     <br /><br />
                     <h4>JAVA SCRIPT</h4>
                 </div>
+                <div className="bootstrap box" data-aos="fade-up"
+                    data-aos-duration="2200"
+                    data-aos-delay="600">
+                    <img src="./assets/img/bootstrap.png" alt="" />
+                    <br /><br />
+                    <h4>BOOTSTRAP</h4>
+                </div>
                 <div className="reactjs box" data-aos="fade-up"
-                data-aos-duration="3000"
-                data-aos-delay="600">
+                    data-aos-duration="2400"
+                    data-aos-delay="700">
                     <img src="./assets/img/reactjs.png" alt="" />
                     <br /><br />
                     <h4>REACT JS</h4>
                 </div>
                 <div className="bootstrap box" data-aos="fade-up"
-                data-aos-duration="3000"
-                data-aos-delay="700">
-                    <img src="./assets/img/bootstrap.png" alt="" />
+                    data-aos-duration="2600"
+                    data-aos-delay="800">
+                    <img src="./assets/img/nodejsc.png" alt="" />
                     <br /><br />
-                    <h4>BOOTSTRAP</h4>
+                    <h4>NodeJS</h4>
+                </div>
+                <div className="bootstrap box" data-aos="fade-up"
+                    data-aos-duration="2800"
+                    data-aos-delay="900">
+                    <img src="./assets/img/expresjs.png" alt="" />
+                    <br /><br />
+                    <h4>ExpresJS</h4>
+                </div>
+                <div className="bootstrap box" data-aos="fade-up"
+                    data-aos-duration="3000"
+                    data-aos-delay="1000">
+                    <img src="./assets/img/mongodb.png" alt="" />
+                    <br /><br />
+                    <h4>MongoDB</h4>
                 </div>
             </div>
 
